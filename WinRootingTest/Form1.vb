c879240d275bc60984e463e69cf112c6fadaafa3
kazenetu <rakudafactory@gmail.com>
2017-07-14 22:16:07 +0900

    イベント処理の実装を修正

--- WinRootingTest/Form1.vb
+++ WinRootingTest/Form1.vb
@@ -1,11 +1,41 @@
 ﻿Public Class Form1
+    ''' <summary>
+    ''' フォームロード
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' ラベルのイベント設定
+        setHandler(Label1)
 
-    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+    End Sub
+
+    ''' <summary>
+    ''' ラベルのマウスイベントを追加
+    ''' </summary>
+    ''' <param name="target"></param>
+    Private Sub setHandler(target As Label)
+        AddHandler target.MouseDown, AddressOf Label_MouseDown
+        AddHandler target.MouseMove, AddressOf Label_MouseMove
+
+    End Sub
+
+    ''' <summary>
+    ''' ラベル マウスクリック
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Label_MouseDown(sender As Object, e As MouseEventArgs)
         Dim target As Label = DirectCast(sender, Label)
         target.Tag = e.Location
     End Sub
 
-    Private Sub Label_MouseMove(sender As Object, e As MouseEventArgs) Handles Label1.MouseMove
+    ''' <summary>
+    ''' ラベル マウスドラッグ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Label_MouseMove(sender As Object, e As MouseEventArgs)
         If Not e.Button = MouseButtons.Left Then
             Return
         End If
