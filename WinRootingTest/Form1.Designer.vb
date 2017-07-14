7cfe14ba5349fe167ff7ab461da469460de4383a
kazenetu <rakudafactory@gmail.com>
2017-07-14 22:05:45 +0900

    初期コミット

---
+++ WinRootingTest/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.DimGray
+        Me.Label1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(55, 175)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 60)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "開始位置"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(702, 447)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As Label
+End Class
---
+++ WinRootingTest/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+        Dim target As Label = DirectCast(sender, Label)
+        target.Tag = e.Location
+    End Sub
+
+    Private Sub Label_MouseMove(sender As Object, e As MouseEventArgs) Handles Label1.MouseMove
+        If Not e.Button = MouseButtons.Left Then
+            Return
+        End If
+
+        Dim target As Label = DirectCast(sender, Label)
+        Dim basePoint As Point = DirectCast(target.Tag, Point)
+        target.Left = CInt((target.Left + e.Location.X - basePoint.X) / 50) * 50
+        target.Top = CInt((target.Top + e.Location.Y - basePoint.Y) / 50) * 50
+    End Sub
+
+End Class
